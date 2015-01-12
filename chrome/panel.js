09492f8200cff39d38e046ef0ebe2e84d0293d66
David Walsh <davidwalsh83@gmail.com>
2015-01-12 13:43:59 -0600

    Adding add-on code based on Victor's template

---
+++ chrome/panel.js
@@ -0,0 +1,61 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+this.EXPORTED_SYMBOLS = ["MDNDevToolPanel"];
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyModuleGetter(this, "EventEmitter",
+  "resource://gre/modules/devtools/event-emitter.js");
+XPCOMUtils.defineLazyModuleGetter(this, "promise",
+  "resource://gre/modules/commonjs/sdk/core/promise.js", "Promise");
+
+/**
+ * This is the add-on's panel, wrapping the tool's contents.
+ *
+ * @param nsIDOMWindow iframeWindow
+ *        The iframe window containing the tool's markup and logic.
+ * @param Toolbox toolbox
+ *        The developer tools toolbox, containing all tools.
+ */
+function MDNDevToolPanel(iframeWindow, toolbox) {
+  this.panelWin = iframeWindow;
+  this._toolbox = toolbox;
+  EventEmitter.decorate(this);
+};
+
+MDNDevToolPanel.prototype = {
+  get target() this._toolbox.target,
+
+  /**
+   * Open is effectively an asynchronous constructor.
+   * Called when the user select the tool tab.
+   *
+   * @return object
+   *         A promise that is resolved when the tool completes opening.
+   */
+  open: function() {
+    return this.panelWin.startup(this._toolbox, this.target).then(() => {
+      this.isReady = true;
+      this.emit("ready");
+      return this;
+    });
+  },
+
+  /**
+   * Called when the user closes the toolbox or disables the add-on.
+   *
+   * @return object
+   *         A promise that is resolved when the tool completes closing.
+   */
+  destroy: function() {
+    return this.panelWin.shutdown().then(() => {
+      this.isReady = false;
+      this.emit("destroyed");
+    });
+  }
+};
---
+++ chrome/tool.js
@@ -0,0 +1,55 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyModuleGetter(this, "EventEmitter",
+  "resource://gre/modules/devtools/event-emitter.js");
+XPCOMUtils.defineLazyModuleGetter(this, "promise",
+  "resource://gre/modules/commonjs/sdk/core/promise.js", "Promise");
+
+XPCOMUtils.defineLazyGetter(this, "toolStrings", () =>
+  Services.strings.createBundle("chrome://mdn-devtool/locale/strings.properties"));
+
+/**
+ * This file has access to the `window` and `document` objects of the add-on's
+ * iframe, and is included in tool.xul. This is the add-on's controller.
+ */
+
+/**
+ * Called when the user select the tool tab.
+ *
+ * @param Toolbox toolbox
+ *        The developer tools toolbox, containing all tools.
+ * @param object target
+ *        The local or remote target being debugged.
+ * @return object
+ *         A promise that should be resolved when the tool completes opening.
+ */
+function startup(toolbox, target) {
+  // $("#hello").textContent = toolStrings.GetStringFromName("greeting");
+  $("#hello").textContent = toolStrings.formatStringFromName("customizedGreeting", ["MDNDevTool"], 1);
+
+  return promise.resolve();
+}
+
+/**
+ * Called when the user closes the toolbox or disables the add-on.
+ *
+ * @return object
+ *         A promise that should be resolved when the tool completes closing.
+ */
+function shutdown() {
+  return promise.resolve();
+}
+
+/**
+ * DOM query helpers.
+ */
+function $(selector, target = document) target.querySelector(selector);
+function $$(selector, target = document) target.querySelectorAll(selector);
